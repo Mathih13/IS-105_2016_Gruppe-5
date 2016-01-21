123426f93b13e3a6fbec8ecbddaa1fdb9299c0b3
PansexualWalrus <mathih13@uia.no>
2016-01-21 15:14:22 +0100

    v0.1
    Welcome text, call to action and input storage

---
+++ RockPaperScissors.py
@@ -0,0 +1,14 @@
+
+
+
+
+#Welcome Text and Call to Action
+print "Welcome to Rock, Paper, Scissors!"
+print "#####################################"
+print "To play, trype either rock, paper or scissors."
+print ""
+
+playerChoice = raw_input("Your Choice: ")
+
+
+ 
