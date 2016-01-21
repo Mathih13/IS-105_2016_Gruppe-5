ef7eac9bb4267f43dd4975b3a2cea767e8610769
PansexualWalrus <mathih13@uia.no>
2016-01-21 12:53:49 +0100

    Added calculation input

--- Funksjoner-Uke3.py
+++ Funksjoner-Uke3.py
@@ -1,9 +1,15 @@
+calc = input('What operator do you want to use? ')
 
-def multiply():   
+if calc == 'multiplication':
+    multiply()
+elif calc == 'subtraction':
+    subtract()
+
+def multiply():
+    print "Multiplication"
     x = input('Enter first number: ')
     y = input('Enter second number: ')
     sum = int(x) * int(y)
     print sum
     return
 
-multiply()
