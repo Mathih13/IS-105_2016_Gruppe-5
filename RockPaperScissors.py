902f2e4083ebe203cf5c97bf04face9c252de667
erih14 <erik.haaland@hotmail.com>
2016-01-21 15:31:56 +0100

    Type

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -5,7 +5,7 @@
 #Welcome Text and Call to Action
 print "Welcome to Rock, Paper, Scissors!"
 print "#####################################"
-print "To play, trype either rock, paper or scissors."
+print "To play, type either rock, paper or scissors."
 print ""
 
 playerChoice = raw_input("Your Choice: ")
